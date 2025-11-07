b2935cf3e001369dc27b0c20906938dc87256e67
Ajit raut <ajitraut9561@gmail.com>
2025-11-07 23:21:08 +0530

    Add initial migration for users

--- server/create_admin.py
+++ server/create_admin.py
@@ -1,4 +1,11 @@
+# create_admin.py
+import os
+import django
 from django.contrib.auth import get_user_model
+
+# This line is the fix!
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
 User = get_user_model()
 
@@ -7,6 +14,6 @@ password = "Admin@123"
 
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(email=email, password=password)
-    print("Superuser created successfully ✅")
+    print("Superuser created successfully")
 else:
-    print("Superuser already exists ✅")
+    print("Superuser already exists")
